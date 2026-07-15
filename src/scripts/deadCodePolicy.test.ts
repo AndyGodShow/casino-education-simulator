2bcdf8378100dd0f9448ae1c00b4434e5e5074a4
AndyGodShow <AndyGodShow@users.noreply.github.com>
2026-07-15 13:32:12 +0800

    test(quality): record complete dead-code inventory

--- src/scripts/deadCodePolicy.test.ts
+++ src/scripts/deadCodePolicy.test.ts
@@ -16,6 +16,115 @@ const REMOVED_EXPORT_SURFACE: Record<string, string[]> = {
     'StraightNumberStrategy', 'RandomOutsideStrategy',
   ],
   'src/utils/countryNameMap.ts': ['countryNameMap', 'uiVocabularyMap'],
+  'src/utils/dicePips.ts': ['DICE_PIP_PATTERNS'],
+  'src/modules/sports/football/worldCup/logic/oddsEngine.ts': ['normalizeBookProbabilities'],
+  'src/modules/core/trustLayer/dataTruth.ts': [
+    'TRUTH_LEVEL_LABELS', 'TRUTH_LEVEL_CONFIDENCE', 'clampConfidence',
+  ],
+  'src/modules/sports/football/worldCup/alpha/alphaEvaluator.ts': [
+    'computeSignalAttribution', 'BucketResult', 'CalibrationDrift', 'SignalAttribution',
+  ],
+  'src/modules/sports/football/worldCup/alpha/alphaStore.ts': [
+    'MAX_ALPHA_RECORDS', 'defaultAlphaStore', 'OneX2Probability',
+  ],
+  'src/modules/sports/football/worldCup/logic/signalLayer.ts': [
+    'computeSignalLayer', 'computeSignals', 'AlphaSignalQuality',
+  ],
+  'src/modules/sports/football/worldCup/logic/alphaEngine.ts': ['applyAlphaSignalsToLambda'],
+  'src/modules/sports/football/worldCup/logic/scoreDistribution.ts': ['applyDrawMassCorrection'],
+  'src/modules/sports/football/worldCup/logic/consistencyValidator.ts': ['validateBehavioralConstraints'],
+  'src/modules/sports/football/worldCup/data/educationalOdds.ts': ['educationalOdds'],
+  'src/dataProviders/polymarket/adapters.ts': ['marketQualityScore', 'calculateMarketQuality'],
+  'src/dataProviders/football/fixtureProvider.ts': ['WorldCupProviderOutput'],
+  'src/dataProviders/football/types.ts': ['FootballProviderStatus'],
+  'src/modules/sports/football/worldCup/data/publicWorldCupSnapshot.ts': ['PublicWorldCupAdapterResult'],
+  'src/modules/sports/football/worldCup/types.ts': [
+    'IntelligenceFactorSide', 'MatchIntelligenceCoverage', 'PredictionRiskBand',
+    'MatchAdvancedFeatureContribution',
+  ],
+  'src/modules/sports/football/worldCup/domain/WorldCupDomainModel.ts': [
+    'MatchViewModel', 'TeamViewModel', 'MatchDataStaleness',
+  ],
+  'src/components/Common/Simulation/stats.ts': ['BatchTestMethodId'],
+  'src/modules/core/probability/unifiedProbability.ts': ['ProbabilitySource'],
+  'src/modules/sports/football/worldCup/backtest/types.ts': [
+    'WorldCupBacktestPredictionOrigin', 'WorldCupBacktestStageBucket',
+    'WorldCupBacktestEdgeBucket', 'WorldCupBacktestTempoBucket', 'WorldCupBacktestCoverageBucket',
+  ],
+  'src/modules/sports/football/worldCup/backtest/worldCupBacktest.ts': [
+    'WorldCupBacktestBucket', 'WorldCupBacktestCalibrationReadiness', 'WorldCupBacktestMetrics',
+    'WorldCupBacktestSourceTier', 'WorldCupConfidenceBacktestBucket',
+  ],
+  'src/modules/sports/football/worldCup/backtest/combinedAuditSummary.ts': [
+    'WorldCupCombinedCalibrationEvidenceStatus',
+  ],
+  'src/modules/sports/football/worldCup/backtest/historicalBacktest.ts': [
+    'HistoricalBacktestRejectionReason', 'HistoricalBacktestAudit',
+    'HistoricalBacktestCsvRejectionReason', 'HistoricalBacktestCsvAudit',
+  ],
+  'src/modules/sports/football/worldCup/logic/modelConfig.ts': [
+    'WorldCupEvidenceShrinkageMultiplierKey', 'WorldCupEvidenceDrawCorrectionMultiplierKey',
+  ],
+  'src/modules/sports/football/worldCup/backtest/strategyTuning.ts': [
+    'WorldCupStrategyTuningParameter', 'WorldCupStrategyTuningDirection',
+    'WorldCupStrategyTuningRecommendation', 'WorldCupStrategyTuningPatchChange',
+  ],
+  'src/modules/sports/football/worldCup/calibration/alphaPersistence.ts': ['SignalStabilityRanking'],
+  'src/modules/sports/football/worldCup/calibration/calibrationSummary.ts': ['CalibrationGrade'],
+  'src/modules/sports/football/worldCup/calibration/marketAlignment.ts': ['DirectionConsistency'],
+  'src/modules/sports/football/worldCup/calibration/outcomeCalibration.ts': [
+    'CalibrationCurveBucket', 'OverconfidenceReport',
+  ],
+  'src/modules/sports/football/worldCup/logic/bookmakerEngine.ts': ['BookmakerExposure'],
+  'src/modules/sports/football/worldCup/logic/groupMotivation.ts': ['TeamMotivationState'],
+  'src/modules/sports/football/worldCup/logic/poissonModel.ts': ['ScoreProbability'],
+  'src/modules/sports/football/worldCup/logic/providerQualityRegistry.ts': [
+    'FootballProviderFieldCoverage',
+  ],
+  'src/modules/sports/football/worldCup/research/internationalResults.ts': [
+    'InternationalResultsRejectionReason',
+  ],
+  'src/modules/sports/football/worldCup/research/walkForwardOptimizer.ts': [
+    'StrategyEvaluationMetrics',
+  ],
+  'src/games/craps/types.ts': ['CrapsRoundStatus'],
+  'src/components/SimulationPanel/index.ts': ['SimulationPanelProps', 'StatBoxProps'],
+  'src/games/blackjack/types.ts': ['BlackjackHand', 'BlackjackRoundResult'],
+  'src/components/SimulationPanel/SimulationPanel.tsx': [
+    'StrategyOption', 'RunParams', 'ExtraControlsCtx',
+  ],
+  'src/modules/sports/football/worldCup/backtest/index.ts': [
+    'runCombinedWorldCupBacktest', 'buildWorldCupStrategyCalibrationOverrides',
+    'recommendWorldCupStrategyTuning', 'buildHistoricalBacktestDataset',
+    'parseHistoricalBacktestCsv', 'runHistoricalWorldCupBacktest',
+    'LOCAL_SAMPLE_HISTORICAL_BACKTEST_CSV', 'buildStrategyScenarioContext',
+    'optimizeWorldCupStrategy', 'predictStrategyCandidate',
+    'strategyOptimizationSamplesFromTimeline', 'WorldCupBacktestBucket',
+    'WorldCupBacktestCalibrationReadiness', 'WorldCupBacktestCalibrationUsabilityStatus',
+    'WorldCupBacktestMetrics', 'WorldCupBacktestQuality', 'WorldCupBacktestScenarioBuckets',
+    'WorldCupBacktestScenarioProfile', 'WorldCupBacktestStageCoverage',
+    'WorldCupBacktestSourceCoverage', 'WorldCupBacktestSourceTier',
+    'WorldCupCombinedBacktestAudit', 'WorldCupCombinedBacktestOriginAudit',
+    'WorldCupCombinedBacktestRun', 'WorldCupCombinedCalibrationAudit',
+    'WorldCupConfidenceBacktestBucket', 'CombinedWorldCupBacktestInput',
+    'WorldCupBacktestQualitySummary', 'WorldCupCombinedCalibrationEvidenceStatus',
+    'WorldCupCombinedCalibrationSummary', 'CombinedWorldCupCalibrationRun',
+    'WorldCupStrategyTuningDirection', 'WorldCupStrategyTuningPatch',
+    'WorldCupStrategyTuningPatchChange', 'WorldCupStrategyTuningParameter',
+    'WorldCupStrategyTuningRecommendation', 'WorldCupStrategyTuningReport',
+    'HistoricalBacktestAudit', 'HistoricalBacktestCsvAudit', 'HistoricalBacktestCsvParse',
+    'HistoricalBacktestCsvRejectionReason', 'HistoricalBacktestDataset',
+    'HistoricalBacktestImportSummary', 'HistoricalBacktestRejectionReason',
+    'HistoricalBacktestRow', 'StrategyCandidate', 'StrategyEvaluationMetrics',
+    'StrategyOptimizationSample', 'WalkForwardStrategyReport',
+  ],
+  'src/server/worldCup/strategyResearchEndpoint.ts': ['HISTORICAL_RESULTS_URLS'],
+  'src/modules/sports/football/worldCup/hooks/useWorldCupDomain.ts': [
+    'WorldCupDomainRefreshCoordinator',
+  ],
+  'src/modules/sports/football/worldCup/research/strategyResearchSnapshot.ts': [
+    'WorldCupStrategyResearchProvenance',
+  ],
 };
 
 type KnipSymbol = { name: string };
